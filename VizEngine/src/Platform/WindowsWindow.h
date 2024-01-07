c3afbca8f186b9d190225f95cb83fb701b8de8fa
Siva Subramanyam <v.sivavadlamani@gmail.com>
2024-01-07 16:27:33 +0800

    Window System for Cross Platform Implementation (Windows implementation now)
    - Added Window.h for commons
    - Added WindowsWindow for handing glfw window for WindowsOS

---
+++ VizEngine/src/Platform/WindowsWindow.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include"../VizEngine/Window.h"
+#include<iostream>
+#include<string>
+#include <GLFW/glfw3.h>
+#include"../VizEngine/Core.h"
+
+namespace VizEngine
+{
+	class WindowsWindow : public Window
+	{
+	public:
+		WindowsWindow(const WindowProperties& props);
+		virtual ~WindowsWindow();
+
+		void OnUpdate() override;
+
+		inline unsigned int GetWidth() const override { return m_Data.Width; }
+		inline unsigned int GetHeight() const override { return m_Data.Height; }
+
+		inline void SetEventCallback(const EventCallbackFunction& callback) override { m_Data.EventCallback = callback; }
+		void SetVSync(bool enabled);
+		bool IsVSync() const;
+	private:
+		virtual void Init(const WindowProperties& props);
+		virtual void Shutdown();
+	private:
+		GLFWwindow* m_Window;
+
+		struct WindowData
+		{
+			std::string Title;
+			unsigned int Width, Height;
+			bool VSync;
+
+			EventCallbackFunction EventCallback;
+		};
+
+		WindowData m_Data;
+	};
+}
